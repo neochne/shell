c144fd6d6e5a9a80de5b87b8dc84664f02477dee
Neo Chen <neochne@gmail.com>
2026-02-09 16:58:02 +0800

    feat: add delete_finder_recent function

--- mac/mac.sh
+++ mac/mac.sh
@@ -11,6 +11,10 @@ mac_battery() {
     ioreg -rn AppleSmartBattery | grep -i "MaxCapacity\|Designcapacity"
 }
 
+delete_finder_recent() {
+    defaults delete com.apple.finder FXRecentFolders
+}
+
 # ################################## Command #####################################
 
 # 1) 直接执行 mac.sh ssproxyterminal {set|unset|status} 不会起作用，因为方法里涉及到了环境变量，而执行 mac.sh 会开启
